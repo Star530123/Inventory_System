90405bfa32d6854ddc567fb8cf3cbd54e04b485c
YuXing <simon.lin@tpisoftware.com>
2021-09-19 15:30:13 +0800

    test: insert auto increment id test

--- src/test/java/com/ivymon/inventory/InventoryApplicationTests.java
+++ src/test/java/com/ivymon/inventory/InventoryApplicationTests.java
@@ -1,13 +1,24 @@
 package com.ivymon.inventory;
 
+import com.ivymon.inventory.entity.GoodsCategory;
+import com.ivymon.inventory.entity.mapper.GoodsCategoryMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InventoryApplicationTests {
 
+	@Autowired
+	GoodsCategoryMapper goodsCategoryMapper;
+
 	@Test
 	void contextLoads() {
+		GoodsCategory goodsCategory = new GoodsCategory();
+		goodsCategory.setCategoryName("容器");
+		System.out.println("Before:" + goodsCategory.getCategorySeq());
+		goodsCategoryMapper.insertSelective(goodsCategory);
+		System.out.println("After:" + goodsCategory.getCategorySeq());
 	}
 
 }
